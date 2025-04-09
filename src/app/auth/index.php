8473ed4bb6fea7c48fa0f08991199f8462e0648e
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2025-04-09 23:19:13 +0800

    refactor: functions.php support, rmv redundant partials,

--- src/app/auth/index.php
+++ src/app/auth/index.php
@@ -1,64 +1,72 @@
-<?php
-include_once __DIR__ . '/../../utils/php/functions.php';
-$title = 'Sign In';
-?>
+<?php include_once __DIR__ . '/../../utils/php/functions.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<?= shared('partials/top'); ?>
-<div class="shared-standalone-cont">
-	<?= shared('layouts/loader/window'); ?>
-</div>
+<head>
+	<?= shared('elements/meta'); ?>
+	<title>Sign In - Fil Eats</title>
+	<?= shared('elements/styles'); ?>
+</head>
 
-<div class="site-cont">
-	<main class="site-main">
-		<section id="auth">
-			<div class="container">
-				<div class="row justify-content-center">
-					<div class="col-md-5">
-						<div class="auth-wrapper">
-							<?= feature('auth/components/header'); ?>
-							<div class="auth-body shadow mx-auto p-4">
-								<div class="auth-body-header">
-									<h3 class="col_green">Login</h3>
-									<div class="mb-2">Login with your Cooking account</div>
-								</div>
-								<form class="ui form">
-									<div class="field">
-										<label for="email">Email Address</label>
-										<input type="email" name="email" class="form-control" placeholder="Your Email">
+<body>
+	<div class="shared-standalone-cont">
+		<?= shared('layouts/loader/window'); ?>
+	</div>
+
+	<div class="site-cont">
+		<main class="site-main">
+			<section id="auth">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-md-5">
+							<div class="auth-wrapper">
+								<?= featured('auth/components/header'); ?>
+								<div class="auth-body shadow mx-auto p-4">
+									<div class="auth-body-header">
+										<h3 class="col_green">Login</h3>
+										<div class="mb-2">Login with your Cooking account</div>
 									</div>
-									<div class="field">
-										<label for="password">Password</label>
-										<input type="password" name="password" class="form-control"
-											placeholder="Your Password">
-									</div>
-									<div class="field d-flex justify-content-between mt-3">
-										<div class="form-check">
-											<input class="form-check-input" type="checkbox" name="remember" value=""
-												id="remember">
-											<label class="form-check-label" for="remember">
-												Remember Me
-											</label>
+									<form class="ui form">
+										<div class="field">
+											<label for="email">Email Address</label>
+											<input type="email" name="email" class="form-control"
+												placeholder="Your Email">
 										</div>
-										<a href="#">Forgot Password?</a>
-									</div>
-									<div class="actions">
-										<button class="ui submit positive button" type="submit">Login
-											<i class="bi bi-box-arrow-in-right ms-1 align-middle"></i></button>
-									</div>
-								</form>
-								<div class="mt-4 mb-0">Don't have an account? <a class="col_green"
-										href="register.php">Register</a></div>
+										<div class="field">
+											<label for="password">Password</label>
+											<input type="password" name="password" class="form-control"
+												placeholder="Your Password">
+										</div>
+										<div class="field d-flex justify-content-between mt-3">
+											<div class="form-check">
+												<input class="form-check-input" type="checkbox" name="remember" value=""
+													id="remember">
+												<label class="form-check-label" for="remember">
+													Remember Me
+												</label>
+											</div>
+											<a href="#">Forgot Password?</a>
+										</div>
+										<div class="actions">
+											<button class="ui submit positive button" type="submit">Login
+												<i class="bi bi-box-arrow-in-right ms-1 align-middle"></i></button>
+										</div>
+									</form>
+									<div class="mt-4 mb-0">Don't have an account? <a class="col_green"
+											href="register.php">Register</a></div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</section>
-	</main>
-</div>
+			</section>
+		</main>
+	</div>
 
-<!-- scripts -->
-<?= shared('elements/scripts'); ?>
-<script src="<?= feature('auth/login/main.js', true); ?>"></script>
+	<!-- scripts -->
+	<?= shared('elements/scripts'); ?>
+	<script src="<?= featured('auth/login/main.js', true); ?>"></script>
 
-<?= shared('partials/bot'); ?>
+</body>
+
+</html>
--- src/app/auth/register.php
+++ src/app/auth/register.php
@@ -1,68 +1,76 @@
-<?php
-include_once __DIR__ . '/../../utils/php/functions.php';
-$title = 'Sign Up';
-?>
+<?php include_once __DIR__ . '/../../utils/php/functions.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<?= shared('partials/top'); ?>
-<div class="shared-standalone-cont">
-	<?= shared('layouts/loader/window'); ?>
-</div>
+<head>
+	<?= shared('elements/meta'); ?>
+	<title>Sign Up - Fil Eats</title>
+	<?= shared('elements/styles'); ?>
+</head>
 
-<div class="site-cont">
-	<main class="site-main">
-		<section id="auth">
-			<div class="container">
-				<div class="row justify-content-center">
-					<div class="col-md-5">
-						<div class="auth-wrapper">
-							<?= feature('auth/components/header'); ?>
-							<div class="auth-body shadow mx-auto p-4">
-								<div class="auth-body-header">
-									<h3 class="col_green">Register</h3>
-									<div class="mb-2">Create your Cooking account</div>
-								</div>
-								<form class="ui form">
-									<div class="field">
-										<label for="name">Full Name</label>
-										<input type="text" class="form-control" name="name" placeholder="Your Name">
+<body>
+	<div class="shared-standalone-cont">
+		<?= shared('layouts/loader/window'); ?>
+	</div>
+
+	<div class="site-cont">
+		<main class="site-main">
+			<section id="auth">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-md-5">
+							<div class="auth-wrapper">
+								<?= featured('auth/components/header'); ?>
+								<div class="auth-body shadow mx-auto p-4">
+									<div class="auth-body-header">
+										<h3 class="col_green">Register</h3>
+										<div class="mb-2">Create your Cooking account</div>
 									</div>
-									<div class="field">
-										<label for="email">Email Address</label>
-										<input type="email" class="form-control" name="email" placeholder="Your Email">
-									</div>
-									<div class="field">
-										<label for="password">Password</label>
-										<input type="password" class="form-control" name="password"
-											placeholder="Your Password">
-									</div>
-									<div class="field d-flex justify-content-between mt-3">
-										<div class="form-check">
-											<input class="form-check-input" type="checkbox" value="" name="remember"
-												id="remember">
-											<label class="form-check-label" for="remember">
-												I agree with the <a href="#">Terms Of Service</a>
-											</label>
+									<form class="ui form">
+										<div class="field">
+											<label for="name">Full Name</label>
+											<input type="text" class="form-control" name="name" placeholder="Your Name">
 										</div>
+										<div class="field">
+											<label for="email">Email Address</label>
+											<input type="email" class="form-control" name="email"
+												placeholder="Your Email">
+										</div>
+										<div class="field">
+											<label for="password">Password</label>
+											<input type="password" class="form-control" name="password"
+												placeholder="Your Password">
+										</div>
+										<div class="field d-flex justify-content-between mt-3">
+											<div class="form-check">
+												<input class="form-check-input" type="checkbox" value="" name="remember"
+													id="remember">
+												<label class="form-check-label" for="remember">
+													I agree with the <a href="#">Terms Of Service</a>
+												</label>
+											</div>
+										</div>
+										<div class="actions">
+											<button class="ui submit positive button" type="submit">Register
+												<i class="bi bi-arrow-right-circle ms-1 align-middle"></i></button>
+										</div>
+									</form>
+									<div class="mt-4 mb-0">Already have an account? <a class="col_green"
+											href="index.php">Login</a>
 									</div>
-									<div class="actions">
-										<button class="ui submit positive button" type="submit">Register
-											<i class="bi bi-arrow-right-circle ms-1 align-middle"></i></button>
-									</div>
-								</form>
-								<div class="mt-4 mb-0">Already have an account? <a class="col_green"
-										href="index.php">Login</a>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</section>
-	</main>
-</div>
+			</section>
+		</main>
+	</div>
 
-<!-- scripts -->
-<?= shared('elements/scripts'); ?>
-<script src="<?= feature('auth/register/main.js', true); ?>"></script>
+	<!-- scripts -->
+	<?= shared('elements/scripts'); ?>
+	<script src="<?= featured('auth/register/main.js', true); ?>"></script>
 
-<?= shared('partials/bot'); ?>
+</body>
+
+</html>
