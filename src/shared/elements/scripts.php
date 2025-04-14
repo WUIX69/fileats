d23c39c2b1ddaa15eda0ead0f1a800a8264789a5
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2025-04-14 17:48:52 +0800

    update: php 8.4 dependencies

--- src/shared/elements/scripts.php
+++ src/shared/elements/scripts.php
@@ -1,7 +1,7 @@
 <?php
 $scripts = [
     'lib/jquery/jquery.min.js',
-    // 'vendor/bootstrap/dist/js/bootstrap.bundle.min.js', TODO: use all bootstrap except its modal
+    'vendor/bootstrap/dist/js/bootstrap.bundle.min.js', // TODO: use all bootstrap except its modal
     'vendor/fomantic-ui/dist/semantic.min.js',
     'lib/lodash/lodash.min.js',
     'assets/js/loader/window.js',
