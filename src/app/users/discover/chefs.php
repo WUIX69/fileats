a0baaa236acb31d8e1788ddc1b4412c789b39f16
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2025-04-16 00:33:21 +0800

    fix: /discover and header on /users app

--- src/app/users/discover/chefs.php
+++ src/app/users/discover/chefs.php
@@ -1,3 +1,4 @@
+<?php include_once __DIR__ . '/../../../utils/php/functions.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 
--- src/features/users/shared/layouts/header.php
+++ src/features/users/shared/layouts/header.php
@@ -1,3 +1,14 @@
+<?php
+// TODO: move all the global logic in /config dir
+$urlPath = $_SERVER['REQUEST_URI'] ?? null;
+
+global $urlParts;
+$urlParts = explode('/', trim($urlPath, '/')) ?? '';
+$pageName = $urlParts[3] ?? '';
+
+global $activeLink;
+$activeLink = str_replace('.php', '', $pageName);
+?>
 <header class="site-header">
     <nav class="navbar navbar-expand-lg navbar-light w-100 pb-2 border-bottom">
         <div class="container-xl">
@@ -33,33 +44,36 @@
                 <div class="offcanvas-body pt-0 align-items-center">
                     <ul class="navbar-nav align-items-lg-center ms-auto mt-2">
                         <li class="nav-item">
-                            <a class="nav-link active" href="<?= app('users'); ?>" title="Visit home page">
+                            <a class="nav-link <?= ($activeLink === 'index' || $activeLink === '') ? 'active' : '' ?>"
+                                href="<?= app('users'); ?>" title="Visit home page">
                                 Dashboard
                             </a>
                         </li>
                         <li class="nav-item dropdown drop_border">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle <?= $activeLink === 'discover' ? 'active' : '' ?>"
+                                href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
                                 Find Recipes<img src="<?= statf('assets/img/icons-svg/chevron-down.svg'); ?>" width="15"
                                     height="15" alt="Submenu open/close icon">
                             </a>
                             <ul class="dropdown-menu drop_1 shadow" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="<?= app('users/discovers/recipes/'); ?>">Discover
+                                <li><a class="dropdown-item" href="<?= app('users/discover/recipes'); ?>">Discover
                                         Recipes</a></li>
-                                <li><a class="dropdown-item border-0" href="<?= app('users/discovers/chefs/'); ?>">
+                                <li><a class="dropdown-item border-0" href="<?= app('users/discover/chefs'); ?>">
                                         Discover
                                         Chefs</a>
                                 </li>
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="<?= app('users/bookmarks'); ?>" title="Visit Bookmarked recipes">
+                            <a class="nav-link <?= $activeLink === 'bookmarks' ? 'active' : '' ?>"
+                                href="<?= app('users/bookmarks'); ?>" title="Visit Bookmarked recipes">
                                 Bookmarks
                             </a>
                         </li>
                         <li class="nav-item dropdown drop_border">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle <?= $activeLink === 'recipe' ? 'active' : '' ?>" href="#"
+                                id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Manage Recipes<img src="<?= statf('assets/img/icons-svg/chevron-down.svg'); ?>"
                                     width="15" height="15" alt="Submenu open/close icon">
                             </a>
@@ -72,7 +86,8 @@
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="<?= app('users/settings'); ?>" title="Visit Settings">
+                            <a class="nav-link <?= $activeLink === 'settings' ? 'active' : '' ?>"
+                                href="<?= app('users/settings'); ?>" title="Visit Settings">
                                 Settings
                             </a>
                         </li>
@@ -100,11 +115,11 @@
                                     alt="Submenu open/close icon">
                             </a>
                             <ul class="dropdown-menu drop_1 shadow" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="<?= app('users'); ?>/index.html"> Dashboard</a></li>
-                                <li><a class="dropdown-item" href="<?= app('users/settings'); ?>/single-view.html">
+                                <li><a class="dropdown-item" href="<?= app('users'); ?>"> Dashboard</a></li>
+                                <li><a class="dropdown-item" href="<?= app('users/settings'); ?>">
                                         Settings</a>
                                 </li>
-                                <li><a class="dropdown-item border-0" href="<?= app('landing'); ?>/single-view.html">
+                                <li><a class="dropdown-item border-0" href="<?= app('landing'); ?>">
                                         Logout</a>
                                 </li>
                             </ul>
